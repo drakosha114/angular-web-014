d30b78651c018101cf36dd295eddd8623125f27f
drakosha <drakosha114@gmail.com>
2017-04-17 22:03:46 +0300

    added addSection component

--- app/sections.addSection.component.ts
+++ app/sections.addSection.component.ts
@@ -1,3 +1,33 @@
-/**
- * Created by red11 on 4/14/2017.
- */
+import { Component, OnInit, Input } from '@angular/core';
+import { Http, URLSearchParams, QueryEncoder } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
+interface Section {
+    title: string
+}
+
+@Component({
+    selector: 'app-sections-addSection',
+    template: ``
+})
+export class SectionsAddSection {
+
+    private sectionsURl: string = 'http://localhost:8080/notes';
+    section: Section;
+    private text: string;
+
+    constructor (private http: Http) {
+        this.text = '';
+    }
+
+    textareaOnKeyupHandler (text:string):void {
+
+    }
+    addSection () {
+
+    }
+
+
+
+}
